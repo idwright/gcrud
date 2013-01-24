7d2d9aa080e1beaad33a6a1ad2a8529d4b74da88
Ian Wright <ian.wright@well.ox.ac.uk>
2013-01-24 12:58:24 +0000

    Allow for image file upload

--- grails-app/domain/gcrud/Person.groovy
+++ grails-app/domain/gcrud/Person.groovy
@@ -6,11 +6,13 @@ class Person {
 		name(blank:false, maxSize:100)
 		email(url:true)
 		description(maxSize:1500)
+		filename(blank:true, nullable:true)
     }
 	
 	String name
 	String email
 	String description
+	String filename
 	
 	static hasMany = [affiliation:Affiliation]
 	
