e9dd2f32174a6cc987f154d735e1ad84a329be23
Ian Wright <ian.wright@well.ox.ac.uk>
2013-01-25 15:55:43 +0000

    Change type of email

--- grails-app/domain/gcrud/Person.groovy
+++ grails-app/domain/gcrud/Person.groovy
@@ -2,9 +2,12 @@ package gcrud
 
 class Person {
 
+	//expose a json-rest-api
+	static expose = 'person'
+	
     static constraints = {
 		name(blank:false, maxSize:100)
-		email(url:true)
+		email(email:true)
 		description(maxSize:1500)
 		filename(blank:true, nullable:true)
     }
